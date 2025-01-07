38fa1aa673ec01a386a76084d5661e6e775c4e22
fabmnt <fabianmontoya2802@gmail.com>
2025-01-07 12:12:18 -0600

    fix: improve icon URL extraction in getUrlMetadata to support multiple icon link types

--- app/metadata/get-url-metadata.ts
+++ app/metadata/get-url-metadata.ts
@@ -13,10 +13,23 @@ export async function getUrlMetadata(url: string): Promise<URLMetadata> {
   const doc = parser.parseFromString(html, 'text/html')
   const title = doc.querySelector('title')?.textContent ?? undefined
   const description = doc.querySelector('meta[name="description"]')?.getAttribute('content') ?? undefined
-  const icon = doc.querySelector('link[rel="icon"]')?.getAttribute('href') ?? undefined
+  const headLinks = doc.head.getElementsByTagName('link')
+  let iconURL: string | undefined
+  for (const link of Array.from(headLinks)) {
+    const rel = link.getAttribute('rel')
+    if (rel == null) {
+      continue
+    }
+
+    if (rel.includes('icon') || rel.includes('shortcut icon')) {
+      iconURL = link.getAttribute('href') ?? undefined
+      break
+    }
+  }
+
   return {
     title,
     description,
-    iconURL: icon,
+    iconURL,
   }
 }
