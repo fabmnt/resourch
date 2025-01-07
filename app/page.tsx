9ed1e5dc32d7de5460888f12ddf84d541bc1ccea
fabmnt <fabianmontoya2802@gmail.com>
2025-01-07 05:14:07 -0600

    fix: restructure layout for header and search components in Home page for improved usability

--- app/page.tsx
+++ app/page.tsx
@@ -18,20 +18,22 @@ export default async function Home() {
   }
 
   return (
-    <div className='container flex flex-col gap-y-4 md:gap-y-6 mx-auto max-w-4xl py-4 md:py-6'>
-      <header className='flex justify-between items-center'>
-        <div>
-          <h1 className='font-semibold tracking-wide'>Resourch</h1>
-          <p className='text-neutral-300'>A site where you can save your links and resources.</p>
+    <div className='flex flex-col gap-y-4 md:gap-y-6 mx-auto max-w-4xl'>
+      <div className='flex flex-col gap-y-4 sticky top-0 z-50 py-4 md:py-6 bg-background'>
+        <header className='container flex justify-between items-center'>
+          <div>
+            <h1 className='font-semibold tracking-wide'>Resourch</h1>
+            <p className='text-neutral-300'>A site where you can save your links and resources.</p>
+          </div>
+          <div>
+            <CreateResourceDialog />
+          </div>
+        </header>
+        <div className='container'>
+          <SearchResource />
         </div>
-        <div>
-          <CreateResourceDialog />
-        </div>
-      </header>
-      <div>
-        <SearchResource />
       </div>
-      <div className='flex flex-col gap-4'>
+      <div className='container flex flex-col gap-4'>
         {userResources.map((resource) => (
           <Resource
             key={resource.id}
