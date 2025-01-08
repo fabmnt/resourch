03d89abf06dd06b2c11e901de2cd3f323f3e19e5
fabmnt <fabianmontoya2802@gmail.com>
2025-01-07 20:51:29 -0600

    feat: remove sticky positioning from resources layout for improved scrolling behavior

--- app/(resources)/layout.tsx
+++ app/(resources)/layout.tsx
@@ -8,7 +8,7 @@ import { PinnedResources } from './components/pinned-resources'
 export default async function Layout({ children }: Readonly<{ children: ReactNode }>) {
   return (
     <div className='flex flex-col mx-auto max-w-4xl'>
-      <div className='flex flex-col gap-y-4 sticky top-0 z-50 py-4 md:py-6 bg-background'>
+      <div className='flex flex-col gap-y-4 py-4 md:py-6 bg-background'>
         <header className='container flex justify-between items-center'>
           <div>
             <h1 className='font-semibold tracking-wide'>Resourch</h1>
